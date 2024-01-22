e801c0c2b9215a3f4b3f42d731cb74e9394931cf
구환준/모건 <koojun99@naver.com>
2024-01-22 14:37:33 +0900

    Initial commit

---
+++ src/main/java/com/example/ezports/EzportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ezports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzportsApplication.class, args);
+    }
+
+}
