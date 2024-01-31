a54ac6f399c4b4d2e03ab9059c6e495d70bc8ce5
구환준/모건 <koojun99@naver.com>
2024-01-31 14:35:05 +0900

    [FEAT]: Match 엔티티 수정

--- src/main/java/com/example/ezports/domain/match/entity/Match.java
+++ src/main/java/com/example/ezports/domain/match/entity/Match.java
@@ -1,6 +1,7 @@
 package com.example.ezports.domain.match.entity;
 
 import com.example.ezports.domain.common.BaseDateTimeEntity;
+import com.example.ezports.domain.league.entity.League;
 import com.example.ezports.domain.team.entity.Team;
 import jakarta.persistence.*;
 import lombok.*;
@@ -16,6 +17,10 @@ public class Match extends BaseDateTimeEntity {
     private Long id;
 
     private String location;
+
+    @JoinColumn(name = "leagueId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private League league;
 
     @JoinColumn(name = "homeTeamId")
     @ManyToOne(fetch = FetchType.LAZY)
