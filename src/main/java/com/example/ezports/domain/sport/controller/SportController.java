0866505ce1720834d4e2be3c92248c75708c5a15
구환준/모건 <koojun99@naver.com>
2024-01-31 15:09:27 +0900

    [FEAT]: Sport api

--- src/main/java/com/example/ezports/domain/sport/controller/SportController.java
+++ src/main/java/com/example/ezports/domain/sport/controller/SportController.java
@@ -1,4 +1,33 @@
 package com.example.ezports.domain.sport.controller;
 
+import com.example.ezports.domain.sport.service.SportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sports")
+@RequiredArgsConstructor
 public class SportController {
+    private final SportService sportService;
+
+    @PostMapping
+    public void createSport() {
+    }
+
+    @GetMapping
+    public void getSports() {
+    }
+
+    @GetMapping("/{sportId}/leagues")
+    public void getLeagues() {
+    }
+
+    @PutMapping("/{sportId}")
+    public void updateSport() {
+    }
+
+    @DeleteMapping("/{sportId}")
+    public void deleteSport() {
+    }
+
 }
