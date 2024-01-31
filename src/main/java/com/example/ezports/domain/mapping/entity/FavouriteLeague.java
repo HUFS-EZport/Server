e2b2b0de64489d59556b179b7d1380a873c3eb6f
구환준/모건 <koojun99@naver.com>
2024-01-31 14:44:38 +0900

    [FEAT]: Favourite 관련 엔티티

--- src/main/java/com/example/ezports/domain/mapping/entity/FavouriteLeague.java
+++ src/main/java/com/example/ezports/domain/mapping/entity/FavouriteLeague.java
@@ -1,4 +1,25 @@
 package com.example.ezports.domain.mapping.entity;
 
+import com.example.ezports.domain.league.entity.League;
+import com.example.ezports.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FavouriteLeague {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "memberId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @JoinColumn(name = "leagueId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private League league;
 }
--- src/main/java/com/example/ezports/domain/mapping/entity/FavouriteTeam.java
+++ src/main/java/com/example/ezports/domain/mapping/entity/FavouriteTeam.java
@@ -1,4 +1,25 @@
 package com.example.ezports.domain.mapping.entity;
 
+import com.example.ezports.domain.member.entity.Member;
+import com.example.ezports.domain.team.entity.Team;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FavouriteTeam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "memberId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @JoinColumn(name = "teamId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Team team;
 }
