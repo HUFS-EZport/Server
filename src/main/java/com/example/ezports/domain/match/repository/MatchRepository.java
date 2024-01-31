ed8c81b00420816836db7ac41b03f91d55e724a4
구환준/모건 <koojun99@naver.com>
2024-01-31 15:05:49 +0900

    [FEAT]: Match api

--- src/main/java/com/example/ezports/domain/match/repository/MatchRepository.java
+++ src/main/java/com/example/ezports/domain/match/repository/MatchRepository.java
@@ -1,5 +1,6 @@
 package com.example.ezports.domain.match.repository;
 
+import com.example.ezports.domain.match.entity.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/ezports/domain/match/service/MatchCommandService.java
+++ src/main/java/com/example/ezports/domain/match/service/MatchCommandService.java
@@ -1,4 +1,12 @@
 package com.example.ezports.domain.match.service;
 
+import com.example.ezports.domain.match.repository.MatchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MatchCommandService {
+    private final MatchRepository matchRepository;
+
 }
