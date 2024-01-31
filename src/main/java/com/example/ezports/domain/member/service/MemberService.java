da97383df73ae47be2ee95edada226e9d29ebc30
구환준/모건 <koojun99@naver.com>
2024-01-31 20:08:05 +0900

    [FEAT]: Member CRUD- 로그인 제외

--- src/main/java/com/example/ezports/domain/member/service/MemberService.java
+++ src/main/java/com/example/ezports/domain/member/service/MemberService.java
@@ -1,12 +1,51 @@
 package com.example.ezports.domain.member.service;
 
 import com.example.ezports.domain.member.controller.MemberController;
+import com.example.ezports.domain.member.converter.MemberConverter;
+import com.example.ezports.domain.member.dto.MemberRequestDTO;
+import com.example.ezports.domain.member.dto.MemberResponseDTO;
+import com.example.ezports.domain.member.entity.Member;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class MemberService {
-    private final MemberCommandService commandService;
-    private final MemberQueryService queryService;
+    private final MemberCommandService memberCommandService;
+    private final MemberQueryService memberQueryService;
+    private final MemberConverter memberConverter;
+
+    @Transactional
+    public MemberResponseDTO.createMember createMember(MemberRequestDTO.createMember request) {
+        Member member = memberConverter.toMember(request.getEmail(), request.getPassword());
+        Member savedMember = memberCommandService.createMember(member);
+        return memberConverter.toCreateMember(savedMember);
+    }
+
+    @Transactional
+    public MemberResponseDTO.getMember getMember(Long memberId) {
+        Member member = memberQueryService.getMember(memberId);
+        return memberConverter.toGetMember(member);
+    }
+
+    @Transactional
+    public List<MemberResponseDTO.getMember> getMembers() {
+        List<Member> members = memberQueryService.getMembers();
+        return memberConverter.toGetMembers(members);
+    }
+
+    @Transactional
+    public void changePassword(Long memberId, MemberRequestDTO.changePassword request) {
+        Member member = memberQueryService.getMember(memberId);
+        member.changePassword(request.getPassword());
+    }
+
+    @Transactional
+    public void withdraw(Long memberId) {
+        Member member = memberQueryService.getMember(memberId);
+        memberCommandService.withdraw(member);
+    }
 }
